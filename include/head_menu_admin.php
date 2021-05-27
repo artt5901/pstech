1bc1edb6ffcdf69cb683604643707eb89b6c3096
artt5901 <dummy5901@gmail.com>
2021-05-27 14:01:21 +0700

    update v1

--- include/head_menu_admin.php
+++ include/head_menu_admin.php
@@ -135,8 +135,6 @@
   <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
   <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
 
 
   <script>
